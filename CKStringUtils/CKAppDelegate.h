a4fd3ee564130f25a3345f6499982cf5708f22fb
Cody Kimberling <clkimberling@gmail.com>
2013-12-14 09:18:32 -0600

    Initial Commit

---
+++ CKStringUtils/CKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CKAppDelegate.h
+//  CKStringUtils
+//
+//  Created by Cody Kimberling on 12/14/13.
+//  Copyright (c) 2013 Cody Kimberling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CKStringUtils/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CKStringUtils
+//
+//  Created by Cody Kimberling on 12/14/13.
+//  Copyright (c) 2013 Cody Kimberling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CKAppDelegate class]));
+    }
+}
